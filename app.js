09ec8d9f8e9ea1bf1d109eac359188edcb3f3c2c
Hannah Wolfe <github.erisds@gmail.com>
2025-01-25 15:41:03 +0000

    Added a healthcheck endpoint
    - this is an arbitrary change to test auto-deployments

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
