7c810c5308dfc60ae2df61b9d718362d08178e88
Hannah Wolfe <github.erisds@gmail.com>
2025-01-25 15:57:25 +0000

    Updated app to serve basic HTMl
    - moving towards a working thing - let's have a real-ish website!

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const path = require('path');
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/health', (req, res) => {
