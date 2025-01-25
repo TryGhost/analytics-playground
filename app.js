6b745ed9298c277cfe258b7db069bdebd80ecb86
Hannah Wolfe <github.erisds@gmail.com>
2025-01-25 14:15:45 +0000

    Added minimal express server
    - basic hello world app that can respond to requests, as proof of concept

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Main module file
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
